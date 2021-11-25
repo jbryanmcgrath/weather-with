ab8c9c14bee7e60661fa7b69385c39e30303f6cc
jbryanmcgrath <90098243+jbryanmcgrath@users.noreply.github.com>
2021-11-25 18:12:36 -0500

    js functiality to console

--- assets/script.js
+++ assets/script.js
@@ -24,28 +24,47 @@
 // };
 
 var getWeatherBtn = document.querySelector("#getWeatherBtn")
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
 
+today = mm + '/' + dd + '/' + yyyy;
 
 let weather = {
     apikey: "ecf7f3e6bec1200756438570f7b731af",
     fetchWeather: function (userInput) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&units=metric&appid=ecf7f3e6bec1200756438570f7b731af").then((response) => response.json()).then((data) => console.log(data));
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&units=imperial&appid=ecf7f3e6bec1200756438570f7b731af").then((response) => response.json()).then((data) => this.displayWeather(data));
     },
+
     displayWeather: function (data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
+        console.log(name, icon, description, temp, humidity, speed, today)
+        document.querySelector("#city-date-current-weather").innerText = name + "           " + today;
+        document.querySelector("#tempDiv").innerText = "Temp:" + temp + "F";
+        document.querySelector("#windDiv").innerText = "Wind :" + speed + "MPH";
+        document.querySelector("#humidityDiv").innerText = "Humididty: " + humidity + "%";
+        document.querySelector("#descriptionDiv").innerText = "It's " + description;
+        document.querySelector("#iconDiv").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
     }
 };
 
 
-// var getWeatherBtn = document.querySelector("#getWeatherBtn")
+var getWeatherBtn = document.querySelector("#getWeatherBtn")
 
 getWeatherBtn.addEventListener("click", (e) => {
     e.preventDefault();
     var userInput = document.querySelector('#location').value;
     weather.fetchWeather(userInput);
-
+    weatherForecast.fetchWeatherForecast(userInput);
 })
+
+let weatherForecast = {
+    apikey: "ecf7f3e6bec1200756438570f7b731af",
+    fetchWeatherForecast: function (userInput) {
+        fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&cnt=1&units=imperial&appid=ecf7f3e6bec1200756438570f7b731af").then((response) => response.json()).then((data) => console.log(data));
+    }
+}
